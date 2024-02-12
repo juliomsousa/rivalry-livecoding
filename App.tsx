9556e658612df29b66120462c223a76d9c8d8d81
Julio Martins <jm@alterx.ai>
2024-02-12 20:10:38 -0300

    chore: remove duplicated files

--- App.tsx
+++ App.tsx
@@ -2,7 +2,7 @@ import { StatusBar } from 'expo-status-bar';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
-import { Home } from './src/screens/Home';
+import { Home } from '@/screens/Home';
 import { PendingBets } from '@/screens/PendingBets';
 
 const Stack = createNativeStackNavigator();
